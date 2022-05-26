8b4ad882740f090075eb5bea7e2bb2c3a9c19895
y-mattsuda <m.youichi1225@icloud.com>
2022-05-26 11:49:51 +0900

    :green_heart: テスト用のシェルスクリプト追加
    test.shにRとPythonのコードをキックするテスト用のスクリプトを作成

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# setup package
+poetry install
+R -q -e "if (!require('renv')) install.packages('renv')"
+R -q -e "library(renv)"
+R -q -e "renv::restore()"
+
+# run R code
+R -q -e "source('R/run_ts_analysis.r')"
+
+# run Python code
+poetry run python slack/file_sender.py Output/result1.png
+
+# remove image in Output/
+rm Output/result1.png
